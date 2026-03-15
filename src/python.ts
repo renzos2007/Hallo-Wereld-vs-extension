a2ee70abbe2719dcb0e95713bb03a9900dd8aa82
renzos2007 <Renzo@stelleronline.nl>
2026-03-15 17:54:35 +0100

    feat: created a choice for the users to install/enable the missing extensions

--- src/python.ts
+++ src/python.ts
@@ -4,9 +4,9 @@ import * as path from 'path';
 import {checkWorkspace} from './extension';
 
 const extensions = ['ms-python.python', 'ms-python.debugpy', 'ms-python.vscode-python-envs', 'ms-python.vscode-pylance'];
-const needToInstall = [];
+const needToInstallActivate = [];
 
-export function createHelloWorldPython(){
+export async function createHelloWorldPython(){
     const workspace = checkWorkspace();
     if (!workspace){
         return;
@@ -15,12 +15,24 @@ export function createHelloWorldPython(){
     for (let extensionId = 0; extensionId < extensions.length; extensionId++) {
         let extension = vscode.extensions.getExtension(extensions[extensionId]);
         if (!extension) {
-            needToInstall.push(extensionId);
+            needToInstallActivate.push(extensionId);
+        } else{
+            if (!extension.isActive){
+                needToInstallActivate.push(extensionId);
+            }
         }
     }
 
-    if (needToInstall.length > 0) {
-        vscode.window.showInformationMessage('need extensions');
+    if (needToInstallActivate.length > 0) {
+        const choiceString = `This command misses ${needToInstallActivate.length} extensions. These make programming way easier. Do you want to install/enable them? (They are not required)`;
+        const choice = await vscode.window.showInformationMessage(
+        choiceString,
+        'Install/enable'
+        );
+
+        if (choice === 'Install/enable'){
+            vscode.window.showInformationMessage('Installed');
+        }
     }
 
     const rootFolderPath = workspace[0].uri.fsPath;
